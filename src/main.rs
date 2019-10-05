b8ca978f4cb61ff4dbd8674eb24e66adf819a96c
Thomas Campistron <irevoire@hotmail.fr>
2019-10-05 14:12:58 +0900

    use the fractale engine

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-mod color;
 mod mandel;
-mod window;
+use fractal::*;
 
 const HEIGHT: usize = 800;
 const WIDTH: usize = 800;
 
 fn main() {
-    let mut mandel = mandel::Mandel::new(-2.3, -1.4, 10, 250.0);
-    let mut window = window::Window::new(WIDTH, HEIGHT).unwrap();
+    let mut mandel = Update {
+        coord: Coord { x: -2.3, y: -1.4 },
+        iter: 10,
+        zoom: 250.0,
+    };
+    let mut window = Window::new("Mandelbrüst", WIDTH, HEIGHT).unwrap();
 
     // init window
-    mandel.compute(&mut window);
-    window.update();
+    mandel::compute(&mut mandel, &mut window);
+    window.draw(&mandel);
 
     while window.handle_event(&mut mandel) {
-        mandel.compute(&mut window);
+        mandel::compute(&mandel, &mut window);
 
-        window.update();
+        window.draw(&mandel);
     }
 }
--- src/mandel.rs
+++ src/mandel.rs
@@ -1,72 +1,36 @@
-use crate::color;
-use crate::window;
 use rayon::prelude::*;
-use std::time::Instant;
 
-pub struct Pos {
-    pub x: f64,
-    pub y: f64,
-}
+pub fn compute(update: &fractal::Update, window: &mut fractal::Window) {
+    let width = window.width();
 
-pub struct Mandel {
-    pub pos: Pos,
-    pub iter: usize,
-    pub zoom: f64,
-}
+    let x1 = update.coord.x;
+    let y1 = update.coord.y;
 
-impl Mandel {
-    pub fn new(x: f64, y: f64, iter: usize, zoom: f64) -> Self {
-        Mandel {
-            pos: Pos { x, y },
-            iter,
-            zoom,
-        }
-    }
+    window
+        .buffer
+        .par_iter_mut()
+        .enumerate()
+        .for_each(|(index, val)| {
+            let x = index % width;
+            let y = index / width;
 
-    pub fn compute(&self, window: &mut window::Window) {
-        let width = window.width();
+            let c_r = x as f64 / update.zoom + x1;
+            let c_i = y as f64 / update.zoom + y1;
+            let mut z_r = 0.0;
+            let mut z_i = 0.0;
+            let mut i = 0;
 
-        let x1 = self.pos.x;
-        let y1 = self.pos.y;
+            while (z_r * z_r + z_i * z_i < 4.0) && i < update.iter {
+                let tmp = z_r;
+                z_r = z_r * z_r - z_i * z_i + c_r;
+                z_i = 2.0 * z_i * tmp + c_i;
+                i += 1;
+            }
 
-        let now = Instant::now();
-
-        window
-            .buffer
-            .par_iter_mut()
-            .enumerate()
-            .for_each(|(index, val)| {
-                let x = index % width;
-                let y = index / width;
-
-                let c_r = x as f64 / self.zoom + x1;
-                let c_i = y as f64 / self.zoom + y1;
-                let mut z_r = 0.0;
-                let mut z_i = 0.0;
-                let mut i = 0;
-
-                while (z_r * z_r + z_i * z_i < 4.0) && i < self.iter {
-                    let tmp = z_r;
-                    z_r = z_r * z_r - z_i * z_i + c_r;
-                    z_i = 2.0 * z_i * tmp + c_i;
-                    i += 1;
-                }
-
-                if i == self.iter {
-                    *val = 0x0000_0000;
-                } else {
-                    *val = color::hue_to_rgb(
-                        i as f32 * (360.0 / self.iter as f32),
-                        1.0,
-                        i as f32 / self.iter as f32,
-                    );
-                }
-            });
-
-        let duration = now.elapsed();
-        println!(
-            "{:.5} seconds for whatever you did.",
-            duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9
-        );
-    }
+            if i == update.iter {
+                *val = std::u32::MAX;
+            } else {
+                *val = i as u32;
+            }
+        });
 }
