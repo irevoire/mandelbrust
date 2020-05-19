55d7885ac2d59e11743925fe98551a5b1bd8c32d
Thomas Campistron <irevoire@hotmail.fr>
2020-05-19 04:41:11 +0200

    split the main and create a lib

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod color;
+pub mod mandel;
+pub mod window;
+
+pub use mandel::Mandel;
+pub use window::Window;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-mod color;
-mod mandel;
-mod window;
+use mandelbrust::{color, Mandel, Window};
 
 const HEIGHT: usize = 800;
 const WIDTH: usize = 800;
 
 fn main() {
-    let mut mandel = mandel::Mandel::new(-2.3, -1.4, 10, 250.0);
-    let mut window = window::Window::new(WIDTH, HEIGHT).unwrap();
+    let mut mandel = Mandel::new(-1.3, -0.4, 15, 800.0);
+    let mut window = Window::new(WIDTH, HEIGHT).unwrap();
 
     // init window
     let (width, height) = window.dimension();
     mandel.compute(&mut window.buffer, width, height);
     color::convert_nb_to_rbg(mandel.iter, &mut window.buffer);
     window.update();
+    window.update(); // if I don’t do it twice I get a gray screen
 
     while window.handle_event(&mut mandel) {
         let (width, height) = window.dimension();
