96e5a98a86d813a1a1b2a8dc4b7846eedd1bf339
Thomas Campistron <irevoire@hotmail.fr>
2020-05-19 13:53:45 +0200

    add first working version with SIMD

--- src/mandel.rs
+++ src/mandel.rs
@@ -1,3 +1,5 @@
+use faster::*;
+use packed_simd::f64x4;
 use rayon::prelude::*;
 
 pub struct Pos {
@@ -21,28 +23,47 @@ impl Mandel {
     }
 
     pub fn compute(&self, window: &mut [u32], width: usize, _height: usize) {
-        let x1 = self.pos.x;
-        let y1 = self.pos.y;
+        let x1 = f64x4::splat(self.pos.x);
+        let y1 = f64x4::splat(self.pos.y);
 
         let mut chunks: Vec<&mut [u32]> = window.chunks_mut(width).collect();
         chunks.par_iter_mut().enumerate().for_each(|(index, val)| {
-            let y = index;
-            for x in 0..width {
-                let c_r = x as f64 / self.zoom + x1;
-                let c_i = y as f64 / self.zoom + y1;
-                let mut z_r = 0.0;
-                let mut z_i = 0.0;
+            let y = f64x4::splat(index as f64);
+            val.iter_mut().enumerate().for_each(|(i, v)| *v = i as u32);
+            val.simd_iter_mut(u32s(0)).simd_for_each(|v| {
+                let x = f64x4::new(
+                    v.extract(0) as f64,
+                    v.extract(1) as f64,
+                    v.extract(2) as f64,
+                    v.extract(3) as f64,
+                );
+                *v = u32s(0);
+                let c_r = x / f64x4::splat(self.zoom) + x1;
+                let c_i = y / f64x4::splat(self.zoom) + y1;
+                let mut z_r = f64x4::splat(0.0);
+                let mut z_i = f64x4::splat(0.0);
                 let mut i = 0;
+                let mut results = u8s(0);
 
-                while (z_r * z_r + z_i * z_i < 4.0) && i < self.iter {
+                loop {
+                    let partial_res = z_r * z_r + z_i * z_i;
+                    if i == self.iter {
+                        break;
+                    } else if results == u8s(1) {
+                        break;
+                    }
+                    for index in 0..4 {
+                        if results.extract(index) == 0 && partial_res.extract(index) > 4.0 {
+                            *v = v.replace(index, i);
+                            results = results.replace(index, 1);
+                        }
+                    }
                     let tmp = z_r;
                     z_r = z_r * z_r - z_i * z_i + c_r;
                     z_i = 2.0 * z_i * tmp + c_i;
                     i += 1;
                 }
-
-                val[x] = i;
-            }
+            });
         });
     }
 }
