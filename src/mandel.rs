1a9e951a36cca290788f0f7bd35d0e95b41e422c
irevoire <irevoire@hotmail.fr>
2020-05-19 19:02:27 +0200

    try a some pure shit because it's supposed to be faster: it's not

--- src/mandel.rs
+++ src/mandel.rs
@@ -22,50 +22,54 @@ impl Mandel {
         }
     }
 
-    pub fn compute(&self, window: &mut [u32], width: usize, _height: usize) {
+    pub fn compute(&self, window: &mut Vec<u32>, width: usize, _height: usize) {
         let x1 = f64x4::splat(self.pos.x);
         let y1 = f64x4::splat(self.pos.y);
 
-        window
+        *window = window
             .par_chunks_mut(width)
             .enumerate()
-            .for_each(|(index, val)| {
+            .flat_map(|(index, val)| {
                 let y = f64x4::splat(index as f64);
                 val.iter_mut().enumerate().for_each(|(i, v)| *v = i as u32);
-                val.simd_iter_mut(u32s(0)).simd_for_each(|v| {
-                    let x = f64x4::new(
-                        v.extract(0) as f64,
-                        v.extract(1) as f64,
-                        v.extract(2) as f64,
-                        v.extract(3) as f64,
-                    );
-                    *v = u32s(0);
-                    let c_r = x / f64x4::splat(self.zoom) + x1;
-                    let c_i = y / f64x4::splat(self.zoom) + y1;
-                    let mut z_r = f64x4::splat(0.0);
-                    let mut z_i = f64x4::splat(0.0);
-                    let mut i = 0;
-                    let mut results = u8s(0);
+                val.simd_iter(u32s(0))
+                    .simd_map(|mut v| {
+                        let x = f64x4::new(
+                            v.extract(0) as f64,
+                            v.extract(1) as f64,
+                            v.extract(2) as f64,
+                            v.extract(3) as f64,
+                        );
+                        v = u32s(0);
+                        let c_r = x / f64x4::splat(self.zoom) + x1;
+                        let c_i = y / f64x4::splat(self.zoom) + y1;
+                        let mut z_r = f64x4::splat(0.0);
+                        let mut z_i = f64x4::splat(0.0);
+                        let mut i = 0;
+                        let mut results = u8s(0);
 
-                    loop {
-                        let partial_res = z_r * z_r + z_i * z_i;
-                        if i == self.iter {
-                            break;
-                        } else if results == u8s(1) {
-                            break;
-                        }
-                        for index in 0..4 {
-                            if results.extract(index) == 0 && partial_res.extract(index) > 4.0 {
-                                *v = v.replace(index, i);
-                                results = results.replace(index, 1);
+                        loop {
+                            let partial_res = z_r * z_r + z_i * z_i;
+                            if i == self.iter {
+                                break;
+                            } else if results == u8s(1) {
+                                break;
                             }
+                            for index in 0..4 {
+                                if results.extract(index) == 0 && partial_res.extract(index) > 4.0 {
+                                    v = v.replace(index, i);
+                                    results = results.replace(index, 1);
+                                }
+                            }
+                            let tmp = z_r;
+                            z_r = z_r * z_r - z_i * z_i + c_r;
+                            z_i = 2.0 * z_i * tmp + c_i;
+                            i += 1;
                         }
-                        let tmp = z_r;
-                        z_r = z_r * z_r - z_i * z_i + c_r;
-                        z_i = 2.0 * z_i * tmp + c_i;
-                        i += 1;
-                    }
-                });
-            });
+                        v
+                    })
+                    .scalar_collect()
+            })
+            .collect();
     }
 }
