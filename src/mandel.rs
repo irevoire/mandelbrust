9ad0239b68decfabc4d13150bb3267046712d5df
irevoire <irevoire@protonmail.ch>
2020-08-04 19:47:51 +0200

    improve the perf a little bit

--- src/mandel.rs
+++ src/mandel.rs
@@ -1,5 +1,5 @@
 use faster::*;
-use packed_simd::f64x4;
+use packed_simd::{f64x4, m32x4, u32x4, u8x4};
 use rayon::prelude::*;
 
 pub struct Pos {
@@ -32,34 +32,39 @@ impl Mandel {
             .for_each(|(index, val)| {
                 let y = f64x4::splat(index as f64);
                 val.iter_mut().enumerate().for_each(|(i, v)| *v = i as u32);
-                val.simd_iter_mut(u32s(0)).simd_for_each(|v| {
-                    let x = f64x4::new(
-                        v.extract(0) as f64,
-                        v.extract(1) as f64,
-                        v.extract(2) as f64,
-                        v.extract(3) as f64,
-                    );
-                    *v = u32s(0);
+                val.simd_iter_mut(u32x4::splat(0)).simd_for_each(|v| {
+                    let x: f64x4 = unsafe {
+                        f64x4::new(
+                            v.extract_unchecked(0) as f64,
+                            v.extract_unchecked(1) as f64,
+                            v.extract_unchecked(2) as f64,
+                            v.extract_unchecked(3) as f64,
+                        )
+                    };
+                    *v = u32x4::splat(0);
                     let c_r = x / f64x4::splat(self.zoom) + x1;
                     let c_i = y / f64x4::splat(self.zoom) + y1;
                     let mut z_r = f64x4::splat(0.0);
                     let mut z_i = f64x4::splat(0.0);
                     let mut i = 0;
-                    let mut results = u8s(0);
+                    let mut results = u8x4::splat(0);
 
                     loop {
                         let partial_res = z_r * z_r + z_i * z_i;
                         if i == self.iter {
                             break;
-                        } else if results == u8s(1) {
+                        } else if results == u8x4::splat(1) {
                             break;
                         }
-                        for index in 0..4 {
-                            if results.extract(index) == 0 && partial_res.extract(index) > 4.0 {
-                                *v = v.replace(index, i);
-                                results = results.replace(index, 1);
-                            }
-                        }
+                        let mask = results.eq(u8x4::splat(0));
+                        let mask = mask.select(
+                            partial_res.gt(f64x4::splat(4.0)).into(),
+                            m32x4::splat(false),
+                        );
+
+                        *v = mask.select(u32s(i), *v);
+                        results = mask.select(u8x4::splat(i as u8), results);
+
                         let tmp = z_r;
                         z_r = z_r * z_r - z_i * z_i + c_r;
                         z_i = 2.0 * z_i * tmp + c_i;
