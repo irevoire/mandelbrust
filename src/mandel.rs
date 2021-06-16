b1e51e440cc95bf58b45cfd84939146b5e117d31
Tamo <tamo@meilisearch.com>
2021-06-16 20:46:59 +0200

    fix all the dependencies issues

--- src/mandel.rs
+++ src/mandel.rs
@@ -1,4 +1,3 @@
-use faster::*;
 use packed_simd::{f64x4, m32x4, u32x4, u8x4};
 use rayon::prelude::*;
 
@@ -32,7 +31,8 @@ impl Mandel {
             .for_each(|(index, val)| {
                 let y = f64x4::splat(index as f64);
                 val.iter_mut().enumerate().for_each(|(i, v)| *v = i as u32);
-                val.simd_iter_mut(u32x4::splat(0)).simd_for_each(|v| {
+                val.chunks_exact_mut(4).for_each(|data| {
+                    let mut v = u32x4::new(data[0], data[1], data[2], data[3]);
                     let x: f64x4 = unsafe {
                         f64x4::new(
                             v.extract_unchecked(0) as f64,
@@ -41,7 +41,7 @@ impl Mandel {
                             v.extract_unchecked(3) as f64,
                         )
                     };
-                    *v = u32x4::splat(0);
+                    v = u32x4::splat(0);
                     let c_r = x / f64x4::splat(self.zoom) + x1;
                     let c_i = y / f64x4::splat(self.zoom) + y1;
                     let mut z_r = f64x4::splat(0.0);
@@ -62,7 +62,7 @@ impl Mandel {
                             m32x4::splat(false),
                         );
 
-                        *v = mask.select(u32s(i), *v);
+                        v = mask.select(u32x4::splat(i), v);
                         results = mask.select(u8x4::splat(i as u8), results);
 
                         let tmp = z_r;
@@ -70,6 +70,10 @@ impl Mandel {
                         z_i = 2.0 * z_i * tmp + c_i;
                         i += 1;
                     }
+                    data[0] = v.extract(0);
+                    data[1] = v.extract(1);
+                    data[2] = v.extract(2);
+                    data[3] = v.extract(3);
                 });
             });
     }
